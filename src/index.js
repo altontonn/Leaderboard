cf5878f596460d6b089b012eb4b80bc24e3a9872
Newton <altontonnalumasa@gmail.com>
2022-08-23 21:50:34 +0300

    create basic page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import _ from 'lodash';
 import './style.css';
+
+const tables = [
+  {
+    name: 'Name:',
+    score: 100
+  },
+  {
+    name: 'Name:',
+    score: 20,
+  },
+  {
+    name: 'Name:',
+    score: 50,
+  },
+  {
+    name: 'Name:',
+    score: 78,
+  },
+  {
+    name: 'Name:',
+    score: 125,
+  },
+  {
+    name: 'Name:',
+    score: 77,
+  },
+]
+document.querySelector('.table-body').innerHTML = tables.map((table) => (
+  `<tr>
+    <td>${table.name}</td>
+    <td>${table.score}</td>
+  </tr>
+  `
+)).join('');
